59103151f7caa305008c3a68d03aba60fea6639d
Rye Pasaporte <ryepassport@gmail.com>
2024-06-12 08:36:05 +0200

    define the application in main file

--- main.ts
+++ main.ts
@@ -1,51 +1,71 @@
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App } from 'cdktf'
+import { AWSCredentials } from './models/common'
+// import { VpcNetwork } from './constructs/vpc'
+import { UrlShortenerTerraformStateBackendStack } from './stacks/backend'
+import { VpcNetwork } from './stacks/vpc'
+import { STATE_BUCKET_NAME, STATE_DYNAMO_TABLE_NAME, VPC_CIDR } from './util/constants'
+import { EcsApplication } from './stacks/ecs'
 
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    // define resources here
-    // create VPC
-    // create security group
-    // EKS cluster inside VPC
-    // create roles
-    // create policies
-    // create service account
-    // create deployment
-    // create service
-    // create ingress
-    // create autoscaling
-    // create monitoring
-    // create logging
-    // create alerting
-    // create dashboard
-    // create CI/CD pipeline
-    // create DNS
-    // create SSL
-    // create CDN
-    // create WAF
-    // create firewall
-    // create bastion host
-    // create VPN
-    // create NAT gateway
-    // create S3 bucket
-    // create RDS
-    // create DynamoDB
-    // create ElastiCache
-    // create ElasticSearch
-    // create Kinesis
-    // create SQS
-    // create SNS
-    // create Lambda
-    // create Step Functions
-    // create API Gateway
-    // create Cognito
-  }
+const credentials: AWSCredentials = {
+  profile: 'rye-personal',
+  region: 'eu-west-1'
 }
 
-const app = new App();
+const app = new App()
 
-new MyStack(app, "url-shortener-cdktf");
+// S3 backend
+new UrlShortenerTerraformStateBackendStack(app, {
+  credentials,
+  id: 'url-shortener-terraform-state-backend',
+  bucketName: STATE_BUCKET_NAME,
+  dynamoTableName: STATE_DYNAMO_TABLE_NAME
+})
 
-app.synth();
+
+// VPC
+const vpc = new VpcNetwork(app, {
+  credentials,
+  id: 'url-shortener-vpc',
+  cidr: VPC_CIDR,
+  tags: {
+    name: 'url-shortener-vpc',
+    costCenter: 'cost-tag-example-vpc',
+    project: 'url-shortener',
+    service: 'url-shortener-vpc',
+    owner: 'profile-name'
+  }
+})
+
+// ECS application
+const ecsStack = new EcsApplication(app, {
+  credentials,
+  id: 'url-shortener-ecs',
+  vpcNetwork: vpc,
+  tags:{
+    Name: 'url-shortener-ecs',
+    costCenter: 'url-shortener-ecs-cost-center-tag',
+    project: 'url-shortener',
+    service: 'url-shortener-service',
+    owner: credentials.profile as string
+  }
+})
+
+ecsStack.addDependency(vpc)
+
+
+// const eksStack = new EksStack(app, {
+//   credentials,
+//   id: 'url-shortener-eks',
+//   vpcNetwork: vpc,
+//   tags: {
+//     name: 'url-shortener-eks',
+//     costCenter: 'cost-tag-example-eks',
+//     project: 'url-shortener',
+//     service: 'url-shortener-eks',
+//     owner: 'profile-name'
+//   }
+// })
+
+// eksStack.addDependency(vpc)
+
+app.synth()
